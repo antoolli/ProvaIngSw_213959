cc7ddac546410777354f91b8ec2b10a2a154a3cb
antoo <antoo@Antonio>
2022-10-28 16:19:46 +0200

    Creazione test del metodo stringtoint convert controlla i possibili illegal argument

--- ProvaIntermedia/src/test/java/ProvaIntermediaTest/stringToIntConverterTest.java
+++ ProvaIntermedia/src/test/java/ProvaIntermediaTest/stringToIntConverterTest.java
@@ -21,6 +21,24 @@ public class stringToIntConverterTest {
 		assertEquals(50, funny.stringToIntConverter("50"));
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void stringIllegalArgumentMax() {
+		
+		funny.stringToIntConverter("32768");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void stringIllegalArgumentMin() {
+		
+		funny.stringToIntConverter("-32769");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void stringIllegalArgument() {
+		
+		funny.stringToIntConverter("32 ");
+	}
+	
 	
 
 }
