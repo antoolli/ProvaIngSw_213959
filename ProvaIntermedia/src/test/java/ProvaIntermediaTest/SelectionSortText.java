187e2aba0147c6397348d1789f3fc7eb7f3bc97b
antoo <antoo@Antonio>
2022-10-28 16:25:00 +0200

    aggiunta dipendency mockito e creazione selectionSortText

---
+++ ProvaIntermedia/src/test/java/ProvaIntermediaTest/SelectionSortText.java
@@ -0,0 +1,25 @@
+package ProvaIntermediaTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ProvaIntermedia.FunnyAlgorithms;
+
+public class SelectionSortText {
+	
+	private FunnyAlgorithms funny;
+	
+	
+	@Before
+	public void init() {
+		funny= new FunnyAlgorithms();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
